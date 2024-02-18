7718614942308244716e9d6a7e511094254b85fa
sudo.park <sudo.park@kakao.com>
2024-02-18 12:58:09 +0900

    add sample code

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,23 @@
+// The cloud functions fore Firebase SDK to create Cloud functions and trigger
 const functions = require("firebase-functions");
+const {onRequest} = require("firebase-functions/v2/https");
+
+// The firebase Admin SDK to access Firestore
+const firebaseAdmin = require("firebase-admin");
+
+firebaseAdmin.initializeApp();
+
+
+exports.addmessage = functions.https.onRequest(async (req, res) => {
+
+	const original = req.query.text;
+
+	const writeResult = await firebaseAdmin.firestore()
+		.collection("message")
+		.add({original: original})
+
+	res.json({ result: `Message with ID: ${writeResult.id} added.` });
+});
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
