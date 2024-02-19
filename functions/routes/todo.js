c126533c69dde19abd47a8ac922642aa84921171
sudo.park <sudo.park@kakao.com>
2024-02-20 00:21:21 +0900

    [#2] todo 데이터 저장 모델링 및 TodoRouter에서 기본 저장, 조회 기능 테스트
    - todo읙 경우 최상위 collection에 userId넣어서 데이터 저장하도록 변경 -> 유저별로 별도 콜렉션 만들경우 동적으로 계속 index 만들어줘야함

--- functions/routes/todo.js
+++ functions/routes/todo.js
@@ -3,17 +3,18 @@ const express = require("express");
 const router = express.Router();
 const admin = require("firebase-admin");
 
+
 // TODO: 일단은 api 호출시 명시적으로 userId 받도록
 router.get("/:id", async (req, res, next) => {
     try {
-        const snapshot = await admin.firestore().collection('todos').doc(req.params.id).get();
 
-        let todos = [];
-        snapshot.forEach((doc) => {
-            let id = doc.id;
-            let data = doc.data();
-            todos.push({id, ...data});
-        });
+        const snapshot = await admin.firestore()
+            .collection('todos')
+            .where('user_id', '==', req.params.id)
+            .get();
+
+        let todos = snapshot.docs
+            .map(doc => ({id: doc.id, ...doc.data()}) );
     
         res.status(200).send(JSON.stringify(todos));
     } catch(error) {
@@ -24,10 +25,15 @@ router.get("/:id", async (req, res, next) => {
 // TODO: 일단은 api 호출시 명시적으로 userId 받도록
 router.post("/:id", async (req, res, next) => {
 
-    const todo = req.body;
+    let makeParams = {user_id: req.params.id, ...req.body};
 
     try {
-        const snapshot = await admin.firestore().collection('todos').doc(req.params.id).add(todo);
+
+        const ref = await admin.firestore()
+            .collection('todos')
+            .add(makeParams);
+        const snapshot = await ref.get();
+        
         res.status(201).send(
             JSON.stringify({id: snapshot.id, ...snapshot.data()})
         );
