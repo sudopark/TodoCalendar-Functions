6342fa80ddf20f1811041e5d348a6a9f19ca56d8
sudo.park <sudo.park@kakao.com>
2025-06-29 14:01:06 +0900

    [#63] DataChangeLogRepository 구현

--- functions/models/DataChangeLog.js
+++ functions/models/DataChangeLog.js
@@ -10,14 +10,27 @@ DataChangeCase.CREATED = new DataChangeCase('created');
 DataChangeCase.UPDATED = new DataChangeCase('updated');
 DataChangeCase.DELETED = new DataChangeCase('deleted')
 Object.freeze(DataChangeCase);
+Object.freeze(DataChangeCase.CREATED);
+Object.freeze(DataChangeCase.UPDATED);
+Object.freeze(DataChangeCase.DELETED);
 
 
 class DataChangeLog {
 
     constructor(data) {
+        this.uuid = data.uuid
         this.userId = data.userId
         this.changeCase = new DataChangeCase(data.changeCase)
         this.timestamp = data.timestamp
+    }
+
+    toJSON() {
+        return {
+            uuid: this.uuid, 
+            userId: this.userId, 
+            changeCase: this.changeCase.key, 
+            timestamp: this.timestamp
+        }
     }
 }
 
---
+++ functions/repositories/dataChangeLogRepository.js
@@ -0,0 +1,30 @@
+
+
+const { getFirestore } = require('firebase-admin/firestore');
+const db = getFirestore();
+const ChangeLogs = require('../models/DataChangeLog');
+
+class DataChangeLogRepository {
+
+    async findChanges(userId, dataType, timestamp) {
+        const collectionRef = db.collection(`changeLogs_${dataType}`);
+        const query = collectionRef
+            .where('userId', '==', userId)
+            .where('timestamp', '>', timestamp)
+        const snapshot = await query.get();
+        const datas = snapshot.docs.map((d => {
+            return { uuid: d.id, ...d.data() }
+        }))
+        const logs = datas.map((d => ChangeLogs(d)))
+        return logs
+    }
+
+    async updateLog(log, dataType) {
+        const collectionRef = db.collection(`changeLogs_${dataType}`);
+        const { uuid, ...payload } = JSON.stringify(log);
+        const ref = collectionRef.doc(uuid)
+        await ref.set(payload, { merge: false })
+    }
+}
+
+module.exports = DataChangeLogRepository;
