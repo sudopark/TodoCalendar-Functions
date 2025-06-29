c06e687003e63ca24ecee7d857b0693b77e423da
sudo.park <sudo.park@kakao.com>
2025-06-29 12:53:20 +0900

    [#63] SyncTimeStamp, DataChangeLog class 추가

---
+++ functions/models/SyncTimestamp.js
@@ -0,0 +1,11 @@
+
+class SyncTimeStamp {
+
+    constructor(data) {
+        this.userId = data.userId
+        this.dataType = data.dataType
+        this.timestamp = data.timestamp
+    }
+}
+
+module.exports = SyncTimeStamp;
