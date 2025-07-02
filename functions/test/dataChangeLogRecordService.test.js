403430a647443ce2b8b62bf0baeb5b1d1a715b91
sudo.park <sudo.park@kakao.com>
2025-07-03 01:45:47 +0900

    [#63] 로그 저장에 의해 syncTimestamp을 업데이트할떄 기존에 저장된 timestamp 보다 과거 시간인 겨우 syncTimeStamp를 업데이트 안하도록 설정
    - 기기 A에서 데이터 변경을 이르켜 changeLog가 발생하고 이를 syncTimestamp에 반영하려고 하는 경우
    - 이미 저장된 server timestamp가 더 미래의 시간이라면 -> 다른 기기에서 데이터 변경을 일으켰음을 의미
    - 이에 이경우에는 syncTimestamp를 업데이트 하지 않도록 설정

--- functions/test/dataChangeLogRecordService.test.js
+++ functions/test/dataChangeLogRecordService.test.js
@@ -4,6 +4,7 @@ const StubRepos = require('./stubs/stubRepositories');
 const DataChangeLogRecordService = require('../services/dataChangeLogRecordService');
 const ChangeLog = require('../models/DataChangeLog');
 const DataType = require('../models/DataTypes');
+const SyncTimeStamp = require('../models/SyncTimestamp');
 
 describe('DataChangeLogRecordService', () => {
 
@@ -35,6 +36,33 @@ describe('DataChangeLogRecordService', () => {
         
         const syncTime = await syncReposiotry.syncTimestamp('some_user', DataType.EventTag)
         assert.deepEqual(syncTime.timestamp, 100)
+    })
+
+    describe('when server timestamp is future than new log', () => {
+
+        beforeEach(async () => {
+            const timestamp = new SyncTimeStamp('some_user', DataType.EventTag, 200);
+            await syncReposiotry.updateTimestamp(timestamp)
+        })
+
+        it('record log without synctimestamp', async () => {
+            
+            const log = new ChangeLog.DataChangeLog(
+            'some_id', 'some_user', ChangeLog.DataChangeCase.CREATED, 100
+            )
+            await service.record(DataType.EventTag, log)
+
+            const recordLogs = await changeLogRepository.findChanges(
+                'some_user', DataType.EventTag, 99
+            )
+            const recordLog = recordLogs.find(log => {
+                return log.uuid == 'some_id'
+            })
+            assert.deepEqual(recordLog, log)
+            
+            const syncTime = await syncReposiotry.syncTimestamp('some_user', DataType.EventTag)
+            assert.deepEqual(syncTime.timestamp, 200)
+        })
     })
 
     it('when record fail, ignore', async () => {
