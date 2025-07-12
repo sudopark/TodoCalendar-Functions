45d509b1d7a363cd50aeb1d7d03e3cd8e298e02c
sudo.park <sudo.park@kakao.com>
2025-07-12 22:31:09 +0900

    [#64] 대이터 마이그레이션 이후, changeLog 로깅 추가

--- functions/services/migrationService.js
+++ functions/services/migrationService.js
@@ -1,32 +1,61 @@
 
+const { DataChangeCase, DataChangeLog } = require('../models/DataChangeLog');
+const DataTypes = require('../models/DataTypes');
+
 
 class MigrationService {
 
-    constructor(migrationRepository, eventTimeRangeService) {
+    constructor(
+        migrationRepository, 
+        eventTimeRangeService, 
+        changeLogRecordService
+    ) {
         this.migrationRepository = migrationRepository
         this.eventTimeRangeService = eventTimeRangeService
+        this.changeLogRecordService = changeLogRecordService
     }
 
     async migrationEventTags(tags) {
-        return this.migrationRepository.migrateEventTags(tags);
+        await this.migrationRepository.migrateEventTags(tags);
+
+        const logs = []
+        for(const id in tags) {
+            const t = tags[id]
+            const log = new DataChangeLog(id, t.userId, DataChangeCase.CREATED, parseInt(Date.now(), 10))
+            logs.push(log)
+        }
+        
+        await this.changeLogRecordService.recordLogs(DataTypes.EventTag, logs)
     }
 
     async migrationTodos(userId, todos) {
-        const times = new Map();
+        const times = new Map(), logs = [];
+
         for(const id in todos) {
-            const payload = this.eventTimeRangeService.todoEventTimeRange(userId, todos[id]);
+            const todo = todos[id]
+
+            const payload = this.eventTimeRangeService.todoEventTimeRange(userId, todo);
             times.set(id, payload)
+
+            const log = new DataChangeLog(id, todo.userId, DataChangeCase.CREATED, parseInt(Date.now(), 10))
+            logs.push(log)
         }
-        return this.migrationRepository.migratieTodos(todos, times);
+        await this.migrationRepository.migratieTodos(todos, times);
+        await this.changeLogRecordService.recordLogs(DataTypes.Todo, logs)
     }
 
     async migrationSchedules(userId, schedules) {
-        const times = new Map();
+        const times = new Map(), logs = []
+    
         for(const id in schedules) {
-            const payload = this.eventTimeRangeService.scheduleTimeRange(userId, schedules[id]);
+            const event = schedules[id]
+            const payload = this.eventTimeRangeService.scheduleTimeRange(userId, event);
             times.set(id, payload);
+            const log = new DataChangeLog(id, event.userId, DataChangeCase.CREATED, parseInt(Date.now(), 10))
+            logs.push(log)
         }
-        return this.migrationRepository.migrateSchedules(schedules, times);
+        await this.migrationRepository.migrateSchedules(schedules, times);
+        await this.changeLogRecordService.recordLogs(DataTypes.Schedule, logs)
     }
 
     async migrationEventDetails(details) {
